565e7c70277416d511d18f55ae8efc8e0b07e2f1
TechItEasyy <115521160+TechItEasyy@users.noreply.github.com>
2023-06-20 09:27:48 +0530

    Create session-01-assignment.sql

---
+++ session-01/session-01-assignment.sql
@@ -0,0 +1,15 @@
+1. Create Database FaceBook
+2. Create table Account
+	userName - varchar
+	password - varchar
+	age - int
+	height - double
+3. insert 10 rows into Account table with all 4 colum data
+4. insert 2 rows into Account table with only userName and password data
+5. select all the 10 columns from Account table
+6. select userName and height from Account table
+7. delete a colum whose age is 17 years
+8. update one user's password
+9. update one user's age
+10.delete all the colums from Account table
+	
