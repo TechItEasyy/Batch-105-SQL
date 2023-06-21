1f072e5131c4a286eef5e7148dc504d327e2cba9
TechItEasyy <115521160+TechItEasyy@users.noreply.github.com>
2023-06-21 09:13:11 +0530

    Update session-02-assignment.sql

--- session-02/session-02-assignment.sql
+++ session-02/session-02-assignment.sql
@@ -1,1 +1,16 @@
-Test
+1. Give example of 
+	DDL
+	DML
+	DCL
+	TCL
+	DQL 
+2. Create a table Student
+	-rollNumber int not noll
+	-name varchar
+	-section varchar with default as 'C'
+3. Insert 20 students details
+4. Add a column address in the Student table
+5. Modify column rollNumber to double from integer
+6. rename column name to student_name
+7. Difference between delete and truncate
+   Difference between drop and delete 
