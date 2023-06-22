1bbcefd017736e55bce26c7a8133f7741462f254
Md Abid Hussain <128626888+Abidsona19s@users.noreply.github.com>
2023-06-23 03:13:17 +0530

    Abid-session-003.sql

--- session-03/Abid-session-003.sql
+++ session-03/Abid-session-003.sql
@@ -7,10 +7,10 @@ create table Examples(
  -- USING INSERT COMMAND
  
  insert into Examples values('Abid',22);
-  insert into Examples values('shahrukh',24);
-   insert into Examples values('naier',32);
-    insert into Examples values('sona',10);
-    insert into Examples values('baba',40);
+ insert into Examples values('shahrukh',24);
+ insert into Examples values('naier',32);
+ insert into Examples values('sona',10);
+ insert into Examples values('baba',40);
     
     select * from Examples;
     
@@ -20,17 +20,35 @@ create table Examples(
     
         select * from Examples;
 
+   -- USING DELETE WITH WHERE CLAUSE
+
 delete from Examples where age=32;
 
         select * from Examples;
-        
+
+
+        -- USING LIKE
+
+
         select * from Examples where Name like 'f%';
         
                 select * from Examples;
 
+
+           --  USING IN
+
+
 select  Name,age from Examples where Name in('Abid','shahrukh');
 
+
+       -- USING NOT IN
+
+
 select  Name,age from Examples where Name not in('Abid','shahrukh');
+
+
+            -- USING BETWEEN and AND 
+
 
 select  * from Examples where age  between 24 and 22;
 
