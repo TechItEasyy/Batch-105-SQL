bd8ada5dc535ac0676b320b4c15ecf0f59ae79c7
TechItEasyy <115521160+TechItEasyy@users.noreply.github.com>
2023-06-27 09:25:10 +0530

    Update session-06-assignment.sql

--- session-06/session-06-assignment.sql
+++ session-06/session-06-assignment.sql
@@ -1,1 +1,19 @@
-session-04-assignment.sql
+1. Create tables Department and Employee:
+	Department
+		-dept_id int
+		-dept_name varchar
+	Employee
+		-emp_id int
+		-dept_id
+		-emp_name
+		-emp_city
+2. Run query for each one of the joins:
+	-inner join
+	-left join
+	-right join
+	-outer join
+3. What is self join?
+4. table: Student (stName, marks, gender, addressId) 
+          Address (addressId, state, country)
+5. Create view for the students who has scored more than 75 marks and student gender is male
+6. Create a subquery to fetch the details(stName, marks) of the stidents who belong to Bihar.
